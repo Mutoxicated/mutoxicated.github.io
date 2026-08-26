e094690575c2e0db6c7896d09d8687282cefd997
Mutoxicated <blackfalcontv17@gmail.com>
2026-08-26 21:57:24 +0300

    test 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://mutoxicated.github.io",
+  base: "/mutoxicated.github.io",
   fonts: [{
     provider: fontProviders.google(),
     name: "Aldrich",
